4081dc9b9c49e9d6f1518030efc0d98e64904f21
jechmx <jechmx1@gmail.com>
2026-02-05 09:07:46 -0600

    metodo de platzi agregado

--- API/app.py
+++ API/app.py
@@ -10,25 +10,35 @@ def get_familia():
     rows = ["Amin", "Marce", "Miranda"]
     return rows
 
+
 @app.get("/superheroesDC")
 def get_superheroes():
     rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
     return rows
+
+@app.get("/cursosPlatzi")
+def get_cursos():
+    rows = ["Docker", "Bash", "Linux", "Ingles", "Python", "Javascript", "Azure"]
+    return rows
+
 
 @app.get("/superheroesMarvel")
 def get_superheroes_marvel():
     rows = ["Spider-Man", "Iron Man", "Thor", "Hulk", "Black Widow", "Doctor Strange", "Black Panther", "Captain America"]
     return rows
 
+
 @app.get("/LOTRWarriors")
 def get_lotr_warriors():
     rows = ["Frodo", "Sam", "Gandalf", "Aragorn", "Legolas", "Gimli", "Boromir", "Gollum", "El anillo"]
     return rows
 
+
 @app.get("/starwarsWarriors")
 def get_starwars_warriors():
     rows = ['Obiwan', 'el pelón del sable morado', 'Darth Vader', 'El de la colita de caballo', 'Yoda']
     return rows
+
 
 @app.get("/warriorsGOT")
 def get_warriors_got():
